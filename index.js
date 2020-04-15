46ee3f648b429d7515c3b552b767ff88b05944fb
flatironschool <t.benson@live.co.uk>
2020-04-15 13:50:51 +0100

    Done.

--- index.js
+++ index.js
@@ -27,3 +27,21 @@ const titleCased = () => {
 
   return tutorials.map( sentence => capitaliseSentence(sentence) )
 }
+
+// refactored to take capitaliseSentence out of separate function and put as block within map:
+
+const titleCased = () => {
+  const capitaliseWord = (word) => {
+    return word[0].toUpperCase() + word.slice(1)
+  }
+
+  return tutorials.map(sentence => {
+    const words = sentence.split(" ")
+    const capWords = []
+    for (let word of words) {
+      capWords.push(capitaliseWord(word))
+    }
+    return capWords.join(" ")
+  })
+}
+
